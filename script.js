e3fdeefda656562937c2563a63d9ed55e82a5a83
protapkrghosh <protapg518@gmail.com>
2025-03-05 14:39:44 +0600

    complet task count implement

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ let buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
    button.addEventListener("click", function (e) {
       window.alert("Board updated successfully");
-      e.target.setAttribute('disabled', true);
-      e.target.classList.remove('cursor-pointer');
+      e.target.setAttribute("disabled", true);
+      e.target.classList.remove("cursor-pointer");
 
       const taskCount = document.querySelector(".taskAssignedCount");
       let taskCountNum = Number(taskCount.textContent);
-      if (taskCountNum > 0) {
-         taskCountNum--;
-         taskCount.textContent = taskCountNum;
-      }
+      taskCountNum--;
+      taskCount.textContent = taskCountNum;
 
       const completeTask = document.querySelector(".completedTask");
       let completeTaskCount = Number(completeTask.textContent);
       completeTaskCount++;
       completeTask.textContent = completeTaskCount;
 
+      const cardTitle =
+         e.target.parentNode.parentNode.querySelector(".cardTitle");
+      console.log(cardTitle.textContent);
+
       clickCount++;
-      console.log(clickCount);
       if (clickCount === 6) {
          window.alert("Congrates!!! You have completed all the current task");
          clickCount = 0;
