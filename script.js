34232c76460da33473e45880dbe3067fa4e06e1e
protapkrghosh <protapg518@gmail.com>
2025-03-05 15:10:47 +0600

    clear activity history

--- script.js
+++ script.js
@@ -16,16 +16,29 @@ for (let button of buttons) {
       completeTaskCount++;
       completeTask.textContent = completeTaskCount;
 
+      // Add activity task history
       const cardTitle =
-         e.target.parentNode.parentNode.querySelector(".cardTitle");
-      console.log(cardTitle.textContent);
+         e.target.parentNode.parentNode.querySelector(".cardTitle").textContent;
 
+      const activityHistory = document.getElementById("activityHistory");
+      const div = document.createElement("div");
+      div.innerHTML = `
+         <p class="text-[#000] text-[13px] text-start rounded-md bg-[#F4F7FF] p-3">You have Complete The Task ${cardTitle} at 12:48:15 PM</p>
+      `;
+      activityHistory.appendChild(div);
+
+      // Open last alert
       clickCount++;
       if (clickCount === 6) {
-         window.alert("Congrates!!! You have completed all the current task");
+         window.alert("Congrats!!! You have completed all the current task");
          clickCount = 0;
       }
+
+      // Clear activity history
+      document
+         .querySelector(".clearHistory")
+         .addEventListener("click", function () {
+            activityHistory.textContent = "";
+         });
    });
 }
-
-
