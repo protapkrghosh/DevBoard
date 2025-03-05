4452a65fd8c802ecd40e0fcc00578d5187425e3e
protapkrghosh <protapg518@gmail.com>
2025-03-05 14:14:51 +0600

    implement disabled button

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Connected...');
+let clickCount = 0;
+let buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+   button.addEventListener("click", function (e) {
+      window.alert("Board updated successfully");
+      e.target.setAttribute('disabled', true);
+      e.target.classList.remove('cursor-pointer');
+
+      const taskCount = document.querySelector(".taskAssignedCount");
+      let taskCountNum = Number(taskCount.textContent);
+      if (taskCountNum > 0) {
+         taskCountNum--;
+         taskCount.textContent = taskCountNum;
+      }
+
+
+      clickCount++;
+      console.log(clickCount);
+      if (clickCount === 6) {
+         window.alert("Congrates!!! You have completed all the current task");
+         clickCount = 0;
+      }
+   });
+}
+
+
